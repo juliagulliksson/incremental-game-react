12a774a064ffaae1583fd5e78de83c9b8f2824e8
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2018-05-22 20:21:18 +0200

    Added GMO factory upgrade

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import AvocadoTree from './upgrades/Tree';
 import DoubleClicker from './upgrades/DoubleClicker';
 import AvocadoFarmer from './upgrades/Farmer';
 import AvocadoFactory from './upgrades/Factory';
+import GMOFactory from './upgrades/GMOFactory';
 
 export default class Counter extends Component {
 
@@ -14,24 +15,25 @@ export default class Counter extends Component {
     doubleClickerUpgrade: false,
     farmerUpgrade: false,
     factoryUpgrade: false,
+    GMOUpgrade: false,
     treeCost: 10,
     farmerCost: 15,
     factoryCost: 15,
+    GMOCost: 15,
     doubleClickerCost: 10,
     doubleClickCount: 0,
-    incrementClickerInterval: 0,
     amountOfTrees: 0,
     amountOfDoubleClickers: 0,
     amountOfFarmers: 0,
-    amountOfFactories: 0
+    amountOfFactories: 0,
+    amountOfGMOFactories: 0
   } 
 
   componentDidMount() {
     setInterval(() => {
-      this.incrementClicker();
+      this.incrementCounter();
       this.checkUpgradeAvailability();
     }, 100);
-
   }
 
   /* handleClick = () => {
@@ -43,11 +45,11 @@ export default class Counter extends Component {
 
   checkUpgradeAvailability = () => {
     
-     if(this.state.counter >= this.state.doubleClickerCost){
+    if(this.state.counter >= this.state.doubleClickerCost){
       this.setState({ doubleClickerUpgrade: true });
     } else {
       this.setState({ doubleClickerUpgrade: false });
-    } 
+    }
 
     if(this.state.counter >= this.state.treeCost){
       this.setState({ treeUpgrade: true });
@@ -56,33 +58,41 @@ export default class Counter extends Component {
     } 
 
     if(this.state.counter >= this.state.farmerCost){
-      this.setState({ farmerUpgrade: true })
+      this.setState({ farmerUpgrade: true });
     } else {
-      this.setState({ farmerUpgrade: false })
+      this.setState({ farmerUpgrade: false });
     }
 
     if(this.state.counter >= this.state.factoryCost){
       this.setState({ factoryUpgrade: true });
     } else {
       this.setState({ factoryUpgrade: false});
-    } 
+    }
+
+    if(this.state.counter >= this.state.GMOCost){
+      this.setState({ GMOUpgrade: true });
+    } else {
+      this.setState({ GMOUpgrade: false });
+    }
   }
 
-  incrementClicker = () => {
-    //Set counter to the amount of upgrades bought to increment accordingly
+  incrementCounter = () => {
+    //Set counter to the amount of upgrades bought, to increment accordingly
     this.setState({
       counter: this.state.counter +
       // The counter will be added to every ten seconds, thereof the / 10000
       (1 * this.state.amountOfTrees * (100/10000))
-      + // The counter will update every second, to render 1 avocado/second
-      (1 * this.state.amountOfFarmers * (100/1000))
+      // The counter will update every second, to render 1 avocado/second
+      + (1 * this.state.amountOfFarmers * (100/1000))
+      // 8 avocados per second
       + (1 * this.state.amountOfFactories * (100/125))
-      /*+ 16 avocados per second(1 * this.state.amountOfGMOFarms * (100/62.5))
-      + 32 avocados per second(1 * this.state.spaceFactories * (100/31.25))  */
-    })
+      //16 avocados per second
+      + (1 * this.state.amountOfGMOFactories * (100/62.5))
+      /*+ 32 avocados per second(1 * this.state.spaceFactories * (100/31.25))  */
+    });
   }
 
-  incrementCounter = () => {
+  handleClick = () => {
     if (this.state.doubleClickCount > 0){
       this.setState({ counter: this.state.counter + this.state.doubleClickCount });
     } else {
@@ -115,14 +125,13 @@ export default class Counter extends Component {
     }
   }
   
-   buyAvocadoFarmer = () => {
+  buyAvocadoFarmer = () => {
     this.setState({
       counter: this.state.counter - this.state.farmerCost,
       farmerCost: Math.floor(this.state.farmerCost * 1.5),
       amountOfFarmers: this.state.amountOfFarmers + 1
     });
   }
-  
 
   buyAvocadoFactory = () => {
     this.setState({
@@ -131,49 +140,53 @@ export default class Counter extends Component {
       amountOfFactories: this.state.amountOfFactories + 1
     });
   }
-/*
+
   buyGMOfactory = () => {
     this.setState({
-      counter: this.state.counter - 40,
-      incrementClickerInterval: this.state.incrementClickerInterval + 40
+      counter: this.state.counter - this.state.GMOCost,
+      GMOCost: Math.floor(this.state.GMOCost * 1.5),
+      amountOfGMOFactories: this.state.amountOfGMOFactories + 1
     });
-  } */
+  }
 
   render(){
     return(
 
-        <div className="container">
-          <div className="row">
-            
-            <Avocado  handleClick={ this.incrementCounter } 
-                      counter={this.state.counter} />
-            
-            <div className="col-8">
-            
-              <AvocadoTree  Class = { this.state.treeUpgrade } 
-                            Cost = { this.state.treeCost } 
-                            handleClick = { this.buyAvocadoTree }
-                            Amount = { this.state.amountOfTrees}
-                            />
+      <div className="container">
+        <div className="row">
+          
+          <Avocado  handleClick={ this.handleClick } 
+                    counter={this.state.counter} />
+          
+          <div className="col-8">
+          
+            <AvocadoTree  Class = { this.state.treeUpgrade } 
+                          Cost = { this.state.treeCost } 
+                          handleClick = { this.buyAvocadoTree }
+                          Amount = { this.state.amountOfTrees} />
 
-              <DoubleClicker  Cost = { this.state.doubleClickerCost } 
-                              handleClick = { this.buyDoubleClicker }
-                              Class = { this.state.doubleClickerUpgrade }
-                              Amount = { this.state.amountOfDoubleClickers }
-                              />
+            <DoubleClicker  Cost = { this.state.doubleClickerCost } 
+                            handleClick = { this.buyDoubleClicker }
+                            Class = { this.state.doubleClickerUpgrade }
+                            Amount = { this.state.amountOfDoubleClickers } />
 
-              <AvocadoFarmer  Cost = { this.state.farmerCost }
-                              handleClick = { this.buyAvocadoFarmer }
-                              Class = { this.state.farmerUpgrade }
-                              Amount = { this.state.amountOfFarmers } />
+            <AvocadoFarmer  Cost = { this.state.farmerCost }
+                            handleClick = { this.buyAvocadoFarmer }
+                            Class = { this.state.farmerUpgrade }
+                            Amount = { this.state.amountOfFarmers } />
 
-              <AvocadoFactory Cost = { this.state.factoryCost }
-                              handleClick = {this.buyAvocadoFactory}
-                              Class = { this.state.factoryUpgrade }
-                              Amount = { this.state.amountOfFactories }/>
-            </div>
+            <AvocadoFactory Cost = { this.state.factoryCost }
+                            handleClick = {this.buyAvocadoFactory}
+                            Class = { this.state.factoryUpgrade }
+                            Amount = { this.state.amountOfFactories } />
+
+            <GMOFactory Cost = { this.state.GMOCost }
+                        handleClick = {this.buyGMOfactory}
+                        Class = { this.state.GMOUpgrade }
+                        Amount = { this.state.amountOfGMOFactories }/>
           </div>
         </div>
+      </div>
     )
   }
 }
--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function InputField({ handleSubmit, value, placeholder }) {
 
   return (
-    <input  type="text" 
-            placeholder={placeholder} 
-            onKeyDown={handleSubmit} 
-            value={value} />
+    <input  type = "text" 
+            placeholder = { placeholder } 
+            onKeyDown = { handleSubmit }
+            value = { value } />
   )
 }
